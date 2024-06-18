7169d747519d2c1494be703ac09f98259879f6e1
scented-candle <ksnow028@uottawa.ca>
2024-06-17 21:08:58 -0400

    Added the ability to select and reroll die

--- public/assets/dice.js
+++ public/assets/dice.js
@@ -7,10 +7,11 @@ class Dice {
             'docs/design_system/dice3.png',
             'docs/design_system/dice4.png',
             'docs/design_system/dice5.png',
-            'docs/design_system/dice6.png' // Fixed this to dice6.png
+            'docs/design_system/dice6.png'
         ];
+        this.selected = false; //Determines if the dice has been selected or not
     }
-
+   
     rollDie() {
         this.value = Math.floor(1 + Math.random() * 6);
     }
@@ -22,21 +23,48 @@ class Dice {
     getValue() {
         return this.value;
     }
-}
+}      
 
 const diceContainer = document.getElementById("dice-container");
+const rerollButton = document.getElementById("reroll-button");
+let diceArray = [];
+
 
 function createDice() {
+
+    diceArray = [];
+
     for (let i = 0; i < 5; i++) {
         const die = new Dice();
         die.rollDie();
         
         const dieElement = document.createElement('img');
         dieElement.src = die.displayDie();
-        
+        dieElement.className = 'die';
+        dieElement.addEventListener('click', () => toggleSelectDie(die,dieElement));
+
+        diceArray.push(die);
         diceContainer.appendChild(dieElement);
     }
 }
 
+function toggleSelectDie(die, dieElement){ //Toggles the selected property
+    die.selected = !die.selected;
+    dieElement.classList.toggle('selected', die.selected);
+}
+
+function rerollSelectedDie() {
+    for(i = 0; i< diceArray.length; i++){
+        if(diceArray[i].selected){
+
+            diceArray[i].rollDie();
+            diceContainer.children[i].src = diceArray[i].displayDie(); //Updates the image on the screen
+            diceArray[i].selected = false; //Reset the status of the die
+            diceContainer.children[i].classList.remove('selected') //Remove the highlighted status on the die
+        }
+    }
+}
 
 createDice();
+
+rerollButton.addEventListener('click', rerollSelectedDie);
