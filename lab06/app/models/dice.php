7723dd4bcf07530c6cb7192f378f7055da5d25e2
Madelin Rioux <madelinnrioux@gmail.com>
2024-06-26 13:22:18 -0400

    Implemented Dice class and tested with index.php

---
+++ lab06/app/models/dice.php
@@ -0,0 +1,19 @@
+<?php
+namespace Yatzy;
+
+class Dice
+{
+    public $min;
+    public $max;
+
+    public function __construct($min = 1, $max = 6)
+    {
+        $this->min = $min;
+        $this->max = $max;
+    }
+
+    public function roll()
+    {
+        return rand($this->min, $this->max);
+    }
+}
